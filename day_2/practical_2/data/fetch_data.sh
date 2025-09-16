cf57b298376e4da8bd6b9df63e15026a417007ec
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2025-09-16 16:18:15 +0200

    Fetch data for practical2

---
+++ day_2/practical_2/data/fetch_data.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set -euo pipefail
+
+VIZ_DATA="V1_Adult_Mouse_Brain_data"
+if [ -d "$VIZ_DATA" ]; then
+    echo "${VIZ_DATA}: Data directory already exists. Skipping download."
+else
+    echo "Downloading V1_Adult_Mouse_Brain data."
+    mkdir -p "$VIZ_DATA"
+    BASE_URL="https://cf.10xgenomics.com/samples/spatial-exp/1.1.0/V1_Adult_Mouse_Brain"
+
+    FILES=(
+        "V1_Adult_Mouse_Brain_molecule_info.h5"
+        "V1_Adult_Mouse_Brain_filtered_feature_bc_matrix.h5"
+        "V1_Adult_Mouse_Brain_raw_feature_bc_matrix.h5"
+        "V1_Adult_Mouse_Brain_analysis.tar.gz"
+        "V1_Adult_Mouse_Brain_spatial.tar.gz"
+        "V1_Adult_Mouse_Brain_metrics_summary.csv"
+        "V1_Adult_Mouse_Brain_web_summary.html"
+        "V1_Adult_Mouse_Brain_cloupe.cloupe"
+    )
+
+    for file in "${FILES[@]}"; do
+        wget -nc -O "$VIZ_DATA/$file" "$BASE_URL/$file"
+    done
+    echo "Data download complete."
+
+    echo "Extracting downloaded data..."
+    tar -xvzf "$VIZ_DATA/V1_Adult_Mouse_Brain_spatial.tar.gz" -C "$VIZ_DATA"
+    echo "Extraction complete."
+fi
